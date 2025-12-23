358a34fe9eff155dbc0bd4b9b8cc075f8e27a034
Pham Quoc Trieu <trieup920@gmail.com>
2025-12-23 15:54:50 +0700

    Fix SDK version constraint for GitHub Actions deployment

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "16d26eb519f4e9e3ea9a403f56480427
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "569c8cd102f6a1af7ee25e3b79c6b0d0",
+"flutter_bootstrap.js": "568b5a23e4b058372771088678f7d857",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
